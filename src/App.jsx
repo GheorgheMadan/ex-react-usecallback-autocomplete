28184eda7ae54a5d2578fde3be30651ad31fb8dc
Gheorghe <madangheorghe.30@gmail.com>
2025-05-13 15:38:54 +0200

    feat: enhance product search functionality with input validation and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -15,39 +15,52 @@ import './App.css'
 // }
 
 function App() {
+  // creo lo stato per l'input 
   const [query, setQuery] = useState('')
+  // creo lo stato dei prodotti 
   const [products, setProducts] = useState([])
+  // creo lo stato del prodotto
   const [product, setProduct] = useState(null)
 
 
   const handleSearch = async (query) => {
+    // se l'input è vuoto e non contiene spazi allora viene chiuso il menu a tendina
     if (query.trim() === '') {
       setProducts([])
       return
     }
     try {
+      // eseguo la chiamata di ricerca del prodotto
       const res = await fetch(`http://localhost:5000/products?search=${query.toLowerCase()}`)
       const resData = await res.json()
       setProducts(resData)
-      console.log('api');
-
     } catch (error) {
       console.error(error);
     }
   }
 
+  // aggiungo il deobunce che aiuta a fermare il fetch per 500 ms per dare il tempo all'utente di inserire l'input, utilizzo useCallback per evitare di ricreare la funzione ad ogni render
+  // e quindi di evitare di fare più chiamate al server
   const debouncedSearch = useCallback(debounce(handleSearch, 500), [])
 
+  // utilizzo useEffect per eseguire la funzione debouncedSearch quando l'input cambia
+  // e quindi quando l'utente scrive qualcosa
+  // in questo modo evito di fare più chiamate al server
   useEffect(() => {
     debouncedSearch(query)
   }, [query])
 
+  // utilizzo useEffect per eseguire la funzione fetchProduct per recuperare il prodotto
+  // quando l'utente clicca su un prodotto
   const fetchProduct = async (id) => {
     try {
       const res = await fetch(`http://localhost:5000/products/${id}`)
       const data = await res.json()
+      // resetto lo stato dell'input per chiudere il menu a tendina
       setQuery('')
+      // resetto lo stato dei prodotti per chiudere il menu a tendina
       setProducts([])
+      // setto lo stato del prodotto
       setProduct(data)
     } catch (err) {
       console.error(err);
